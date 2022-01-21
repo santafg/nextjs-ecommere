b88a5c50caabb06aca349dcdb5a78b7ef6cb6778
santafg <santanughosh28111998@gmail.com>
2022-01-21 21:19:49 +0530

    product details page added

--- components/Layout.js
+++ components/Layout.js
@@ -1,18 +1,39 @@
 import React from "react";
 import Head from "next/head";
-import { AppBar, Toolbar, Typography, Container } from "@material-ui/core";
+import NextLink from "next/link";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Container,
+  Link,
+} from "@material-ui/core";
 import useStyles from "../utils/styles";
 
-export default function Layout({ children }) {
+export default function Layout({ title, description, children }) {
   const classes = useStyles();
   return (
     <div>
       <Head>
-        <title>Next Ecommerce</title>
+        <title>{title ? `${title} - Next Ecommerce` : "Next Ecommerce"}</title>
+        {description && <meta name="description" content={description}></meta>}
       </Head>
       <AppBar position="static" className={classes.navbar}>
         <Toolbar>
-          <Typography>Ecommerce</Typography>
+          <NextLink href="/" passHref>
+            <Link>
+              <Typography className={classes.brand}>Ecommerce</Typography>
+            </Link>
+          </NextLink>
+          <div className={classes.grow}></div>
+          <div>
+            <NextLink href="/cart" passHref>
+              <Link>Cart</Link>
+            </NextLink>
+            <NextLink href="/login" passHref>
+              <Link>Login</Link>
+            </NextLink>
+          </div>
         </Toolbar>
       </AppBar>
       <Container className={classes.main}>{children}</Container>
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
-import styles from "../styles/Home.module.css";
-import Head from "next/head";
-import Image from "next/image";
+import {
+  Grid,
+  Card,
+  CardActionArea,
+  CardMedia,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+} from "@material-ui/core";
+import NextLink from "next/link";
 import Layout from "../components/Layout";
+import data from "../utils/data";
 
 export default function Home() {
   return (
@@ -9,11 +18,32 @@ export default function Home() {
       <Layout>
         <div>
           <h1>Products</h1>
-          <ul>
-            <li>Product 1</li>
-            <li>Product 2</li>
-            <li>Product 3</li>
-          </ul>
+          <Grid container spacing={3}>
+            {data.products.map((product) => (
+              <Grid item md={4} key={product.name}>
+                <Card>
+                  <NextLink href={`/product/${product.slug}`} passHref>
+                    <CardActionArea>
+                      <CardMedia
+                        component="img"
+                        image={product.image}
+                        title={product.name}
+                      ></CardMedia>
+                      <CardContent>
+                        <Typography>{product.name}</Typography>
+                      </CardContent>
+                    </CardActionArea>
+                  </NextLink>
+                  <CardActions>
+                    <Typography>${product.price}</Typography>
+                    <Button size="small" color="primary">
+                      Add to cart
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
         </div>
       </Layout>
     </>
